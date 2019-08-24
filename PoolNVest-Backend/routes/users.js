95149ef2e41fa0e8614b232f7c85dc6f0196466b
G-Gautam <35315739+ggupta24@users.noreply.github.com>
2019-08-24 16:14:29 -0400

    Fixed release

--- PoolNVest-Backend/routes/users.js
+++ PoolNVest-Backend/routes/users.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const _ = require('lodash');
 const {User, validate} = require('../models/user.js');
 const mongoose = require('mongoose');
@@ -13,8 +13,8 @@ router.post('/', async (req,res) => {
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['username', 'password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+    // const salt = await bcrypt.genSalt(10);
+    // user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
     res.send(_.pick(user, ['username', 'password']));
