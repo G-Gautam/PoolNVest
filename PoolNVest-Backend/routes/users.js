45d0dd5ebe9d6c40f24d1acdb221ed2e9f610fb5
Maneth <maneth.kulatunge@mail.mcgill.ca>
2019-08-24 14:18:41 -0400

    Headers added

--- PoolNVest-Backend/routes/users.js
+++ PoolNVest-Backend/routes/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {User, validate} = require('../models/user.js');
@@ -17,7 +19,8 @@ router.post('/', async (req,res) => {
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
-    res.send(_.pick(user, ['username', 'password']));
+    const token = jwt.sign({_username: user._username}, config.get('jwtPrivateKey'));
+    res.header('x-auth-token', token).send(_.pick(user, ['username', 'password']));
 });
 
 module.exports = router;
