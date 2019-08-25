f02708e1b903eb9e60e85a011eaec071e08e5d46
G-Gautam <35315739+ggupta24@users.noreply.github.com>
2019-08-24 22:42:33 -0400

    Added register for user

--- PoolNVest-Backend/routes/users.js
+++ PoolNVest-Backend/routes/users.js
@@ -1,15 +1,15 @@
 const bcrypt = require('bcrypt-nodejs');
 const _ = require('lodash');
-const {User, validate} = require('../models/user.js');
+const { User, validate } = require('../models/user.js');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ username : req.body.username });
+    let user = await User.findOne({ username: req.body.username });
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['username', 'password']));
@@ -24,5 +24,18 @@ router.post('/', async (req,res) => {
 
     res.send(_.pick(user, ['username', 'password']));
 });
+
+router.get('/GetUser/:username', async (req, res) => {
+    let user = await User.findOne({ username: req.params.username });
+    if (user) {
+        console.log(user);
+        res.json({
+            "user": user
+        });
+    }
+    else {
+        res.send("User not found");
+    }
+})
 
 module.exports = router;
